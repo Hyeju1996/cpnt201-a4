68a4d1ed5c36741deb5c3189dbe0eba2961cb336
Hyeju <hyeju5891@gmail.com>
2022-03-01 18:59:14 -0800

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <Tutorial/>
+<body class="bg-indigo-700">
+  <h1 class="text-5xl text-white">
+  Hello world
+  </h1>
+  <p class="text-white">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti itaque soluta, labore ea quos amet illo perferendis fugit beatae mollitia culpa quaerat. Veritatis aperiam harum placeat, et delectus autem possimus.
+  </p>
+</body>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+</style>
